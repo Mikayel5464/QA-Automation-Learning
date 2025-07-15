2ba967f9c0b82ee4e0c85c8d0b3d9b131507c015
Mikayel MURADYAN <mikayel.muradyan@amadeus.com>
2025-07-15 16:13:17 +0400

    adding FunctionalTest.cs

---
+++ SeleniumLearning/SeleniumLearning/FunctionalTest.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    public class FunctionalTest
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            _driver = new ChromeDriver();
+
+            // _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            _driver.Manage().Window.Maximize();
+            _driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [Test]
+        public void Dropdown()
+        {
+            IWebElement dropdown = _driver.FindElement(By.CssSelector("select.form-control"));
+
+            SelectElement s = new SelectElement(dropdown);
+            s.SelectByText("Teacher");
+
+            Thread.Sleep(3000);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
